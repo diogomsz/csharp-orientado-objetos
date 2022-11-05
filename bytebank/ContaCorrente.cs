0c65a761a43957bfd5a7fd9ecd0c8a01b84b8692
Diogo Martins de Assis <diogoassis3301@gmail.com>
2022-11-05 14:26:53 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numeroAgencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,15 @@
+﻿using bytebank;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContaCorrente contaDoAndre = new ContaCorrente();
+        contaDoAndre.titular = "André Silva";
+        contaDoAndre.numeroAgencia = 15;
+        contaDoAndre.conta = "1010-X";
+        contaDoAndre.saldo = 100;
+
+        Console.WriteLine("Saldo da conta do André: " + contaDoAndre.saldo);
+    }
+}
